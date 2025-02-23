4f62d05b6689e721f35497bacf636827abef8e25
shin-yeseong <1020blue@dongguk.edu>
2025-02-24 02:02:13 +0900

    tailwind css 환경설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
-import SignUp from './pages/sign-up'; 
-import SignIn from './pages/sign-in';
-import MyPage from './pages/mypage';
-
+import SignUp from "./pages/sign-up";
+import SignIn from "./pages/sign-in";
+import MyPage from "./pages/mypage";
+import "./index.css"; 
 function App() {
   return (
-    <Router>  
+    <Router>
       <Routes>
-        <Route path="/" element={<Main />} />  
+        <Route path="/" element={<Main />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/mypage" element={<MyPage/>} />
-
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
